0a92c19d3f0b4839c79ccc02302d5395400a44ac
anis <smail.a.anis@gmail.com>
2020-11-26 18:59:24 +0100

    add firebase analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import firebase from "firebase/app";
+import "firebase/analytics";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWZT1NarIiQhSZKTESLNmg8dqkEZcDYhk",
+  authDomain: "metroquizz.firebaseapp.com",
+  databaseURL: "https://metroquizz.firebaseio.com",
+  projectId: "metroquizz",
+  storageBucket: "metroquizz.appspot.com",
+  messagingSenderId: "599668118503",
+  appId: "1:599668118503:web:9fd44ff46e795a3986e342",
+  measurementId: "G-1LHV4LYX5P"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
